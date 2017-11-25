55cbddba8b2edc48ce565472ae8e6abacf5bd3ce
Jose Quan <quanjosej@yahoo.com>
2017-11-25 10:43:40 -0500

    Fix firebase naming error

--- ChatChat/AppDelegate.swift
+++ ChatChat/AppDelegate.swift
@@ -29,7 +29,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-    FIRDatabase.database().persistenceEnabled = true
+    Database.database().isPersistenceEnabled = true
     FirebaseApp.configure()
     return true
   }
